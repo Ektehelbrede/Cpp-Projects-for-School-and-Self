1839431f8736f2cac6beb1593f60cde884c1cc8a
Michael Mckee <michael.mckee92@gmail.com>
2016-09-20 12:07:22 -0400

    Project Euler Problem 1
    Solution to problem 1 including the ability for the user to set the two
    numbers for multiples, and the number to check below.

---
+++ Project Euler/Problem 1/SumOfMultiples.cpp
@@ -0,0 +1,57 @@
+/*
+ Problem 1: Multiples of 3 and 5
+
+ If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+ get 3, 5, 6, and 9. The sum of these multiples is 23.
+
+ Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+
+/*
+ * This program computes the sum of all of the multiples of 3 or 5 below 1000.
+ *
+ * [In future changes, pass or be prompted for values to use as 'multiples'
+ * as well as giving an upper bound for the sum to be computed for (currently
+ * 1000).]
+ *
+ * @author Michael Mckee 2014
+ */
+
+ #include <iostream>
+
+ using std::cout;
+ using std::cin;
+ using std::endl;
+
+ int main()
+ {
+   int FIRSTMULTIPLE;
+   int SECONDMULTIPLE;
+   int BELOW;
+
+   // Obtain user inputs for the first and second multiples to check, and the
+   // number to check below.
+   cout << "Please input the first multiple: " << endl;
+   cin >> FIRSTMULTIPLE;
+
+   cout << "Please input the second multiple: " << endl;
+   cin >> SECONDMULTIPLE;
+
+   cout << "Please input the number to check below: " << endl;
+   cin >> BELOW;
+
+   int sum = 0;   // Sum initialized to zero, to allow adding.
+
+   for (int i = 0; i < BELOW; i++)
+   {
+     if ((i % FIRSTMULTIPLE == 0) || (i % SECONDMULTIPLE == 0))
+     {
+       sum += i;
+     }
+   }
+
+   cout << "The sum of all the multiples of " << FIRSTMULTIPLE << " and "
+    << SECONDMULTIPLE << " below " << BELOW << " is: " << sum << endl;
+
+   return 0;
+ }
